610cb62488a3b14c8f4b73d78cf5c9035293c49b
Selloni <vip.petrusev@mail.ru>
2022-11-01 18:18:35 +0300

    использование функции usestate

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,50 @@
 import './Header.scss';
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 function HeadearLine() {
   const find = <FontAwesomeIcon icon={faMagnifyingGlass} />;
+
+  const [activeIndex, setIndex] = useState(0);
+  const onClickIndex = (index) => {
+    setIndex(index);
+  };
   return (
     <div class="background-line-top">
       <div class="header-line">
-        <h3 class="navigation-panel-no-delite">FujeFilm</h3>
+        <h3 onClick={() => onClickIndex(0)} class="navigation-panel-no-delite">
+          FujeFilm
+        </h3>
         <div>
-          <a class="navigation-panel">Фильмы</a>
-          <a class="navigation-panel">Сериалы</a>
-          <a class="navigation-panel">Мультфильмы</a>
-          <a class="navigation-panel">Аниме</a>
+          <a
+            onClick={() => onClickIndex(1)}
+            class={activeIndex == 1 ? 'navigation-panel-activ' : 'navigation-panel'}>
+            Фильмы
+          </a>
+          <a
+            onClick={() => onClickIndex(2)}
+            class={activeIndex == 2 ? 'navigation-panel-activ' : 'navigation-panel'}>
+            Сериалы
+          </a>
+          <a
+            onClick={() => onClickIndex(3)}
+            class={activeIndex == 3 ? 'navigation-panel-activ' : 'navigation-panel'}>
+            Мультфильмы
+          </a>
+          <a
+            onClick={() => onClickIndex(4)}
+            class={activeIndex == 4 ? 'navigation-panel-activ' : 'navigation-panel'}>
+            Аниме
+          </a>
         </div>
-        <a class="find"> {find} Поиск</a>
-        <a class="button">Login</a>
+        <a onClick={() => onClickIndex(0)} class="find">
+          {' '}
+          {find} Поиск
+        </a>
+        <a onClick={() => onClickIndex(0)} class="button">
+          Login
+        </a>
       </div>
     </div>
   );
