6987c504e44983d22e75ea157358e3885fe57fac
Selloni <vip.petrusev@mail.ru>
2022-11-04 01:53:50 +0300

    не большая анимация

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,44 +5,35 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 function HeadearLine() {
   const find = <FontAwesomeIcon icon={faMagnifyingGlass} />;
-
-  const [activeIndex, setIndex] = useState(0);
+  // для отслеживания клика
+  const [activeIndex, setIndex] = useState(5);
   const onClickIndex = (index) => {
     setIndex(index);
   };
+  //достаю из массива перменые с каждым обращением через map
+  const val_genre = ['Фильмы', 'Сериалы', 'Мульфильмы', 'Аниме'];
   return (
     <div class="background-line-top">
       <div class="header-line">
-        <h3 onClick={() => onClickIndex(0)} class="navigation-panel-no-delite">
+        <h3 onClick={() => onClickIndex(5)} class="navigation-panel-no-delite">
           FujeFilm
         </h3>
+        <div className="burger"></div>
         <div>
-          <a
-            onClick={() => onClickIndex(1)}
-            class={activeIndex == 1 ? 'navigation-panel-activ' : 'navigation-panel'}>
-            Фильмы
-          </a>
-          <a
-            onClick={() => onClickIndex(2)}
-            class={activeIndex == 2 ? 'navigation-panel-activ' : 'navigation-panel'}>
-            Сериалы
-          </a>
-          <a
-            onClick={() => onClickIndex(3)}
-            class={activeIndex == 3 ? 'navigation-panel-activ' : 'navigation-panel'}>
-            Мультфильмы
-          </a>
-          <a
-            onClick={() => onClickIndex(4)}
-            class={activeIndex == 4 ? 'navigation-panel-activ' : 'navigation-panel'}>
-            Аниме
-          </a>
+          {val_genre.map((value, i) => (
+            <a
+              key={i} //  для рендеринга при работе с map
+              onClick={() => onClickIndex(i)}
+              class={activeIndex === i ? 'navigation-panel-activ' : 'navigation-panel'}>
+              {value}
+            </a>
+          ))}
         </div>
-        <a onClick={() => onClickIndex(0)} class="find">
-          {' '}
+
+        <a onClick={() => onClickIndex(5)} class="find">
           {find} Поиск
         </a>
-        <a onClick={() => onClickIndex(0)} class="button">
+        <a onClick={() => onClickIndex(5)} class="button">
           Login
         </a>
       </div>
